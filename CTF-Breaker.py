27f1627a9ee688412e5b10932f5970f48af47082
Justin Frasier <cerealkiller1918@gmail.com>
2022-11-15 18:30:55 -0600

    the port scanner

--- CTF-Breaker.py
+++ CTF-Breaker.py
@@ -1,14 +1,48 @@
 #! /usr/bin/python3 
 
-import socket
+import sys
+from socket import *
 from datetime import datetime
+import requests
+import io 
 
 def port_scanner(ip_address):
     
-    target = socket.gethostbyname(ip_address)
-
+    target = gethostbyname(ip_address)
+    start_time = datetime.now()
     # Banner
     print("-"*50)
     print("Scanning Target:",target)
-    print("Scanning started at:",str(datetime.now()))
+    print("Scanning started at:",str(start_time))
+    print("-"*50)
 
+    try:
+        # TODO Make a list to pass to Nmap 
+        # Scans for open ports
+        for port in range(1,65535):
+            s = socket(AF_INET,SOCK_STREAM)
+            setdefaulttimeout(1)
+            result = s.connect_ex((target,port))
+            if result == 0:
+                print("Port",port,"is open")
+            s.close()
+
+        ended_time = datetime.now()
+        # Ending Banner
+        print("-"*50)
+        print("Scanned Target:",target)
+        print("Scanning end at:",str(ended_time))
+        print("The Scan took", str(ended_time-start_time),"to run")
+        print("-"*50)
+
+    except KeyboardInterrupt:
+        print("\n Exiting Program !!!!!\n")
+        sys.exit()
+    except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+    except socket.error:
+        print("\ Server not responding !!!!")
+        sys.exit()
+
+port_scanner("192.168.1.14")
